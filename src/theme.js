4f8b777d5be968f0602bf3c01e9f65e4766c08a4
Alain Chea <alain.chea@efrei.net>
2022-05-02 14:50:05 +0200

    ajout theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        contrastText: "#ffffff",
+        main: '#0000CD',
+      },
+      secondary: {
+        main: '#edf2ff',
+      },
+    },
+  });
+
+
+ export default theme
